1b3d678142edf59471016c9add6a5cffdab83053
chairulanam612 <chairulanam612@gmail.com>
2022-04-12 15:44:31 +0700

    lecture 48

--- src/App.js
+++ src/App.js
@@ -1,19 +1,22 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import NewPlace from "./places/pages/NewPlace";
+import MainNavigation from "./shared/components/Navigation/MainNavigation";
 import Users from "./user/pages/Users";
 
 const App = () => {
   return (
     <Router>
+      <MainNavigation />
       {/* <nav>
         <Link to="/">Users</Link>
       </nav> */}
-      <Routes>
-        <Route path="/" element={<Users />} />
-        <Route path="/places/new" element={<NewPlace />} />
-      </Routes>
+      <main>
+        <Routes>
+          <Route path="/" element={<Users />} />
+          <Route path="/places/new" element={<NewPlace />} />
+        </Routes>
+      </main>
     </Router>
   );
 };
-//menit 6:45 lecture 44
 export default App;
