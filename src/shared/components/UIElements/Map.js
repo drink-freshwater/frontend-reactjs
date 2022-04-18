beddf62bf4cca4b75e2cd6e84ef28c4916c7dc40
chairulanam612 <chairulanam612@gmail.com>
2022-04-18 15:47:11 +0700

    pake leaflet buat map

--- src/shared/components/UIElements/Map.js
+++ src/shared/components/UIElements/Map.js
@@ -1,17 +1,24 @@
 import React, { useEffect, useRef } from "react";
+import "leaflet/dist/leaflet.css";
+import "leaflet-defaulticon-compatibility/dist/leaflet-defaulticon-compatibility.webpack.css";
+import Leaflet from "leaflet";
+import "leaflet-defaulticon-compatibility";
 
 import "./Map.css";
 
 const Map = (props) => {
   const mapRef = useRef();
   const { center, zoom } = props;
-  useEffect(() => {
-    const map = new window.google.maps.Map(mapRef.current, {
-      center: center,
-      zoom: zoom,
-    });
 
-    new window.google.maps.Marker({ position: center, map: map });
+  useEffect(() => {
+    const map = Leaflet.map(mapRef.current).setView(center, zoom);
+
+    Leaflet.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+      attribution:
+        '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+    }).addTo(map);
+
+    Leaflet.marker(center).addTo(map);
   }, [center, zoom]);
 
   return (
@@ -19,6 +26,7 @@ const Map = (props) => {
       ref={mapRef}
       className={`map ${props.className}`}
       style={props.style}
+      // id="map"
     ></div>
   );
 };
